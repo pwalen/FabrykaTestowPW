f6e109e0fb5847729da4a8ca153521a4470955f7
pwalen <pawel.walenda@gmail.com>
2020-08-19 19:29:30 +0200

    19-08-2020 19:29

---
+++ scripts/decorator_2.py
@@ -0,0 +1,36 @@
+# dekoratory to sposób aby dodać coś do możliwości funkcji ale bez zmiany tej funkcji
+# jest to ważne przy złożonych projektach, gdzie ingerencje we wnętrze funkcji
+# mogą wprowadzić niepożądane rezultaty w innych miejscach kodu
+
+
+# def regular_function():
+#     print('this is a regular function')
+#
+#
+# def decor(function):
+#     def inside():
+#         print('decorate function')
+#         return function()
+#     return inside
+#
+#
+# new_function = decor(regular_function)
+# new_function()
+#
+# print('\n==============\n')
+
+# * prostszy sposób *
+
+def dekor(function):
+    def inside():
+        print('decorate function')
+        return function()
+    return inside
+
+@dekor
+def regular_function():
+    print('this is a regular function')
+
+regular_function()
+
+
