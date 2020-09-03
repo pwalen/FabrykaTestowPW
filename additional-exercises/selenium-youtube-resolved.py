9a451846845911cc44846472f512483820e52c46
pwalen <pawel.walenda@gmail.com>
2020-09-03 17:38:25 +0200

    03-09-2020 17:38 YT

--- additional-exercises/selenium-youtube-resolved.py
+++ additional-exercises/selenium-youtube-resolved.py
@@ -11,9 +11,10 @@ WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.XPATH,
                                 '/html/body/ytd-app/ytd-popup-container/paper-dialog/yt-upsell-dialog-renderer/div/div[3]/div[1]/yt-button-renderer/a/paper-button'))).click()
 
-# ! zamykamy drugie wyskakujące okienko “Before you continue”, klikając na “I AGREE”. 🍪
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,
-                                                            '/html/body/div/c-wiz/div[2]/div/div/div/div/div[2]/form/div/div[2]'))).click()
+# ! przeskakujemy do drugiego wyskakującego okienka  “Before you continue”
+WebDriverWait(driver, 10).until(
+    EC.frame_to_be_available_and_switch_to_it(driver.find_element_by_xpath('//*[@id="iframe"]')))
 
-# full xpath - the “I AGREE” button: '/html/body/div/c-wiz/div[2]/div/div/div/div/div[2]/form/div/div[2]'
-# full xpath - the “I AGREE” text (on the button): '/html/body/div/c-wiz/div[2]/div/div/div/div/div[2]/form/div/span/span'
+# ! zamykamy drugie wyskakujące okienko “Before you continue”, klikając na “I AGREE”. 🍪
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME,
+                                                            '#introAgreeButton'))).click()
